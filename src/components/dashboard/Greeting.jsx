04e4e7e66a07230fef1f24ac78825c6ab9a22b34
HyuDev7 <hyu.df7@gmail.com>
2023-02-03 10:40:25 +0900

    changed greetin message

--- src/components/dashboard/Greeting.jsx
+++ src/components/dashboard/Greeting.jsx
@@ -5,15 +5,16 @@ function Greeting() {
     <div className="greeting-board board col-sm-6 col-lg-6 p-3 mb-4">
       <h1>Hi Hyu!</h1>
       <p>
-        Welcome back Hyu. We are glad you are here.<br></br>
+        Welcome back Hyu. We are happy to see you again.<br></br>
         Inspire your work, leave your acheivement.
       </p>
-      <button type="button" class="me-4 btn btn-dark list-border">
+      <figcaption>—Tony Robbins, <cite>The only impossible journey is the one you never begin.</cite></figcaption>
+      {/* <button type="button" class="me-4 btn btn-dark list-border">
         View your tasks
       </button>
       <button type="button" class="btn btn-dark list-border">
         View timeline
-      </button>
+      </button> */}
     </div>
   );
 }
