dc20e42daa459ccf21a2c5d1f328bed2a622fe2c
HyuDev7 <hyu.df7@gmail.com>
2023-02-05 19:29:38 +0900

    made server side logic

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,24 +1,15 @@
 
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(Db);
 
 var _db;
 
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("todoApp");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
+  connectToServer:  async function () {
+     await client.connect();
+     await client.db("todoApp");
+    console.log("Successfully connected to MongoDB."); 
   },
 
   getDb: function () {
