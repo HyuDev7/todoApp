a640c172bbacd521c9ed1867d59a4d1afef273fe
HyuDev7 <hyu.df7@gmail.com>
2023-04-28 23:05:11 +0900

    in adding sign in logic based on sign up logic

--- src/client/routes/SignIn.jsx
+++ src/client/routes/SignIn.jsx
@@ -2,6 +2,44 @@ import React from "react";
 import { Link, Form } from "react-router-dom";
 
 export default function SignIn() {
+  
+  async function onClick(e) {
+    const newProfile = profile;
+    // console.log(typeof newProfile);
+
+    try {
+      const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProfile),
+      });
+
+      if (!response.ok) {
+        const message = `an error occurred : ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+    } catch (error) {
+      window.alert(error);
+      return;
+    }
+
+    e.preventDefault();
+
+    //clear input areas
+    setProfile({
+      name: "",
+      email: "",
+      password: "",
+    });
+
+    //navigate user to success page
+    navigate("/succeedsignup");
+  }
+
+
   return (
     <div>
       <h1>hello! here is sign in page!!</h1>
@@ -14,7 +52,7 @@ export default function SignIn() {
           <label for="password">enter your password:</label>
           <input type="password" name="password" id="password" required />
         </div>
-        <button type="submit" onClick={()=>alert("button was clicked!")}>Sign In!</button>
+        <button type="submit" onClick={onClick}>Sign In!</button>
       </Form>
       <p>No account? Let's sign up from <Link to={"/signup"}>here!</Link></p>
     </div>
